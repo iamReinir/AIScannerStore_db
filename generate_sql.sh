196fb46ee2c5edd271f23c47f713f3ebc6242456
Reinr <blackwhitenxt@gmail.com>
2025-03-25 08:21:32 +0700

    table creation order

--- generate_sql.sh
+++ generate_sql.sh
@@ -14,15 +14,15 @@ cat Tables/product.sql >> $OUTPUT_FILE
 cat Tables/drink_mapping.sql >> $OUTPUT_FILE
 cat Tables/food_mapping.sql >> $OUTPUT_FILE
 cat Tables/customer.sql >> $OUTPUT_FILE
+cat Tables/order.sql >> $OUTPUT_FILE
 cat Tables/product_in_store.sql >> $OUTPUT_FILE
 cat Tables/inventory_note.sql >> $OUTPUT_FILE
-cat Tables/inventory_item.sql >> $OUTPUT_FILE
+cat Tables/inventory_note_item.sql >> $OUTPUT_FILE
 cat Tables/wallet.sql >> $OUTPUT_FILE
 cat Tables/wallet_transaction.sql >> $OUTPUT_FILE
 cat Tables/deposit.sql >> $OUTPUT_FILE
 cat Tables/vnp_transaction.sql >> $OUTPUT_FILE
 cat Tables/pos_device.sql >> $OUTPUT_FILE
-cat Tables/order.sql >> $OUTPUT_FILE
 cat Tables/order_item.sql >> $OUTPUT_FILE
 
 cat Functions/generate_code.sql
